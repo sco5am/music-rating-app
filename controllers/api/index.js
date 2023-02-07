58c102ea8f34c1e50ea9ab58654b5a4189eae9e8
bfrazier <bthehippie2@gmail.com>
2023-02-06 21:25:29 -0500

    add user route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const songRoute = require('./songRoute');
+const userRoute = require('./userRoute');
 
 router.use('/song', songRoute);
+router.use('/user', userRoute);
 
 module.exports = router;
