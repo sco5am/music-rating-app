a8e63acad21228e1a2aed84f5f9eaf8c83484803
Iris <iridian95@live.com>
2023-02-08 19:03:52 -0500

    changing seed file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Song } = require('../models');
 
 const userData = require('./userData.json');
-const postData = require('./postData.json');
+const songData = require('./songData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,9 +12,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const post of postData) {
-    await Post.create({
-      ...post,
+  for (const song of songData) {
+    await Song.create({
+      ...song,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
