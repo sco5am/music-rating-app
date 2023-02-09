866010119c62644e445906ebcc233797f402b046
bfrazier <bthehippie2@gmail.com>
2023-02-08 19:02:31 -0500

    add code for correct log in credentials

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -28,6 +28,16 @@ router.post('/login', async (req, res) => {
       });
       return;
     }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({
+        user: userData,
+        message: 'Log in successful!',
+      });
+    });
   } catch (err) {
     res.status(400).json(err);
   }
