4e432da3bcf47cba821ea1c1d884a4d329860d86
Iris <iridian95@live.com>
2023-02-11 18:38:06 -0500

    f

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: { //miliseconds
@@ -26,7 +27,6 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
