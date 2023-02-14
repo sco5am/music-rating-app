33403a759f1a1459477acef6ea07dc1ff184f932
bfrazier <bthehippie2@gmail.com>
2023-02-14 14:09:23 -0500

    remove options; replace w async function

--- public/js/song-details.js
+++ public/js/song-details.js
@@ -12,10 +12,19 @@ const artist = document
 
 const fullSearch = `${songTitle}%20${artist}`;
 
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'd7c6b4bfefmsh543bab1a89406f9p15f96bjsn733aef5dbccc',
-    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
-  },
-};
+async function search() {
+  const response = await fetch(
+    `https://spotify23.p.rapidapi.com/search/?q=${fullSearch}&type=multi&offset=0&limit=10&numberOfTopResults=5`,
+    {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'd7c6b4bfefmsh543bab1a89406f9p15f96bjsn733aef5dbccc',
+        'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
+      },
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+}
+
+search();
