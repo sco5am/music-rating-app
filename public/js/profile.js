da0ffbd8aff8a189566302078c28914ff4305ac3
Iris <iridian95@live.com>
2023-02-15 18:19:01 -0500

    changed profile js and profile handlebars ,song and edit song handle bars

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,7 +1,7 @@
 
 // deletes song rating 
   const delButtonHandler = async (event) => {
-
+ 
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
   
@@ -21,10 +21,11 @@
     event.preventDefault();
     // console.log(event.target);
     const id = event.target.getAttribute('data-update-id');
-    const score = $('#rate2').data('rateValue');
+    console.log(id);
     const description = document.querySelector('#updated-description-content').value.trim();
-   
-    if (score && description) {
+   const score= $("#rate2").data("setAdditionalData");
+
+    if (score || description) {
       //localhost:3001/api/posts/:id
       const response = await fetch(`/api/song/${id}`, {
         method: 'PUT',
@@ -33,7 +34,7 @@
           'Content-Type': 'application/json',
         },
       });
-  
+  console.log(response)
       if (response.ok) {
         document.location.replace('/profile');
       } else {
@@ -47,6 +48,6 @@
   .querySelector('.song-list')
   .addEventListener('click', delButtonHandler);
 // update
-   document
-   .querySelector('#updateBtn')
-   .addEventListener('click', updateSong);
+  document
+  .querySelector('#updateBtn')
+  .addEventListener('click', updateSong);
